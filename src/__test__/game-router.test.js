4c948eb2fc2b387e0b8eb87dad9c22cf4696b79f
diego-ramos130 <diego.onethreezero@gmail.com>
2018-09-26 18:30:50 -0700

    finished day 1

---
+++ src/__test__/game-router.test.js
@@ -0,0 +1,85 @@
+'use strict';
+
+process.env.PORT = 3000;
+
+const faker = require('faker');
+const superagent = require('superagent');
+const server = require('../lib/server');
+
+const API_URL = `http://localhost:${process.env.PORT}/api/games`;
+
+describe('/api/games', () => {
+  beforeAll(server.start);
+  afterAll(server.end);
+
+  test('should break to 404 in case of $insert_url_that_doesn\'t_exist', () => {
+    return superagent.get(`http://localhost:${process.env.PORT}/aaaaaaahhhhhhhhh`)
+      .then(Promise.reject)
+      .catch((response) => {
+        expect(response.status).toEqual(404);
+      });
+  });
+  test('should give a 200 on a post + json data', () => {
+    return superagent.post(API_URL)
+      .set('Content-Type', 'application/json')
+      .send({
+        game: 'Path of Exile',
+        type: 'Action Role Playing Game',
+      })
+      .then((response) => {
+        expect(response.status).toEqual(200);
+        expect(response.body.game).toEqual('Path of Exile');
+        expect(response.body.type).toEqual('Action Role Playing Game');
+        expect(response.body.timestamp).toBeTruthy();
+        expect(response.body.id).toBeTruthy();
+      });
+  });
+  test('should give a 404 on a get that don\'t exist', () => {
+    return superagent.get(`${API_URL}/LUUUUUUUUUUUL`)
+      .then(Promise.reject)
+      .catch((response) => {
+        expect(response.status).toEqual(404);
+      });
+  });
+  test('should give a 400 on passing a get with nothing in the object', () => {
+    return superagent.get(`${API_URL}/`)
+      .set('Content-Type', 'application/json')
+      .send({
+        game: 'crontent',
+      })
+      .then(Promise.reject)
+      .catch((response) => {
+        expect(response.status).toEqual(400);
+      });
+  });
+  test('should give 200 if you actually request something with a correct id', () => {
+    const originalRequest = {
+      game: faker.lorem.words(2),
+      type: faker.lorem.words(4),
+    };
+    return superagent.post(API_URL)
+      .set('Content-Type', 'application/json')
+      .send(originalRequest)
+      .then((postResponse) => {
+        originalRequest.id = postResponse.body.id;
+        return superagent.get(`${API_URL}/${postResponse.body.id}`);
+      })
+      .then((getResponse) => {
+        expect(getResponse.status).toEqual(200);
+        expect(getResponse.body.timestamp).toBeTruthy();
+        expect(getResponse.body.id).toEqual(originalRequest.id);
+        expect(getResponse.body.game).toEqual(originalRequest.game);
+      });
+  });
+  test('should give 400 if you post without a required field', () => {
+    return superagent.post(API_URL)
+      .set('Content-Type', 'application/json')
+      .send({
+        game: 'Path of Exile',
+      })
+      .then(Promise.reject)
+      .catch((response) => {
+        expect(response.status).toEqual(400);
+      });
+  });
+});
--- src/routes/game-router.js
+++ src/routes/game-router.js
@@ -26,5 +26,49 @@ router.post('/api/games', jsonParser, (request, response, next) => {
   }
   const game = new Game(request.body.game, request.body.type);
   storageById.push(game.id);
-  storageByHash[note.id] = note;
+  storageByHash[game.id] = game;
+
+  logger.log(logger.INFO, 'successful, sending a 200 and returning json object of what user made');
+  logger.log(logger.INFO, storageById);
+  logger.log(logger.INFO, storageByHash);
+  return response.json(game);
+});
+
+router.get('/api/games/:id', jsonParser, (request, response, next) => {
+  logger.log(logger.INFO, `trying to get a game by the id of ${request.params.id}`);
+  if (storageByHash[request.params.id]) {
+    logger.log(logger.INFO, 'successful, responding with a 200 status code and json data');
+    return response.json(storageByHash[request.params.id]);
+  }
+  return next(new HttpError(404, 'The note was not found'));
+});
+router.get('/api/games/', jsonParser, (request, response, next) => {
+  return next(new HttpError(400, 'provide an id'));
+});
+
+router.delete('/api/games/:id', jsonParser, (request, response, next) => {
+  logger.log(logger.INFO, `trying to delete a game by the id of ${request.params.id}`);
+  if (storageByHash[request.params.id]) {
+    logger.log(logger.INFO, 'element found');
+    const idx = storageById.indexOf(request.params.id);
+    storageById.splice(idx, 1);
+    delete storageByHash[request.params.id];
+    return response.sendStatus(204);
+  }
+  return next(new HttpError(404, 'the game wasn\'t found'));
+});
+
+router.put('/api/games/:id', jsonParser, (request, response, next) => {
+  logger.log(logger.INFO, `trying to update a game by the id of ${request.params.id}`);
+  if (storageByHash[request.params.id]) {
+    logger.log(logger.INFO, 'element to be updated found');
+    if (request.body.game) {
+      storageByHash[request.params.id].game = request.body.game;
+    }
+    if (request.body.type) {
+      storageByHash[request.params.id].type = request.body.type;
+    }
+    return response.json(storageByHash[request.params.id]);
+  }
+  return next(new HttpError(404, 'could not find element!'));
 });
